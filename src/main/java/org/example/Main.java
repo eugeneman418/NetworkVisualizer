17fb6221e0ae59a192888e3b1282c9ecd31bf253
wue <wue0913@gmail.com>
2025-03-27 13:25:49 +0100

    added example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,16 @@ package org.example;
 import com.graphhopper.GraphHopper;
 import com.graphhopper.ResponsePath;
 import org.example.routing.Router;
+import org.opencv.core.Point;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         Router router = new Router("netherlands-latest.osm.pbf");
-        ResponsePath responsePath = router.routeCar(51.9243, 4.4700, 51.9910, 4.3649);
-        responsePath.getP
+        ResponsePath responsePath1 = router.routeCar(51.9243, 4.4700, 51.9910, 4.3649);
+        ResponsePath responsePath2 = router.routeCar(51.9243, 4.4700, 51.9910, 4.3660);
+        List<List<Point>> simplified = Simplifier.simplifyNetwork(List.of(responsePath1.getWaypoints(), responsePath2.getWaypoints()));
     }
 }

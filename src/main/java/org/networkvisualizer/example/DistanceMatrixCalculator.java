d9ed2c33f0e64b77495a17f074b4558ddee7e79f
wue <wue0913@gmail.com>
2025-07-02 15:06:06 +0200

    added distance matrix calculator as example

--- src/main/java/org/networkvisualizer/example/DistanceMatrixCalculator.java
+++ src/main/java/org/networkvisualizer/example/DistanceMatrixCalculator.java
@@ -1,6 +1,8 @@
-package org.networkvisualizer.network;
+package org.networkvisualizer.example;
 
 import com.opencsv.CSVWriter;
+import org.networkvisualizer.network.Network;
+import org.networkvisualizer.network.NetworkParser;
 import org.networkvisualizer.routing.Router;
 
 
@@ -9,16 +11,18 @@ import java.io.FileWriter;
 
 public class DistanceMatrixCalculator {
     public static void main(String[] args) throws Exception {
-        String osmPath = args[0];
-        String networkPath = args[1];
-        String outputPath = args[2];
+        String osmPath = "src/test/resources/zh.osm.pbf";
+        String networkPath = "src/test/resources/network.json";
+        String outputPath = "src/main/resources/distance_matrix.csv";
 
         try (CSVWriter writer = new CSVWriter(new FileWriter(outputPath))) {
             writer.writeNext(new String[]{"From", "To", "Mode", "Distance (km)"});
 
             Router router = new Router(osmPath);
             Network network = NetworkParser.parseJson(networkPath);
+
             network.calculateRoutes(router);
+
             for (int i = 0; i < network.edges.size(); i++) {
                 Network.Edge edge = network.edges.get(i);
                 double dist = network.distances.get(i);
